cde8e331e1b6140f891c2d1756e17e88838d701f
Saravana Kumar Phillip Nagarajan <saranpn2705@gmail.com>
2020-05-05 17:20:10 -0700

    Create Visualization.cpp

---
+++ Visualization.cpp
@@ -0,0 +1,31 @@
+void visualization(int n, Robot robot, int step, Robot p[], Robot pr[])
+{
+    //Draw the robot, landmarks, particles and resampled particles on a graph
+
+    //Graph Format
+    plt::title("MCL, step " + to_string(step));
+    plt::xlim(0, 100);
+    plt::ylim(0, 100);
+
+    //Draw particles in green
+    for (int i = 0; i < n; i++) {
+        plt::plot({ p[i].x }, { p[i].y }, "go");
+    }
+
+    //Draw resampled particles in yellow
+    for (int i = 0; i < n; i++) {
+        plt::plot({ pr[i].x }, { pr[i].y }, "yo");
+    }
+
+    //Draw landmarks in red
+    for (int i = 0; i < sizeof(landmarks) / sizeof(landmarks[0]); i++) {
+        plt::plot({ landmarks[i][0] }, { landmarks[i][1] }, "ro");
+    }
+
+    //Draw robot position in blue
+    plt::plot({ robot.x }, { robot.y }, "bo");
+
+    //Save the image and close the plot
+    plt::save("./Images/Step" + to_string(step) + ".png");
+    plt::clf();
+}
